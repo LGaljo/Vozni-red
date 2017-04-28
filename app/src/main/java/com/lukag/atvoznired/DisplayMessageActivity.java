0c47bed4b4ad9ca1c874f1c12d29c416f9b94d92
Luka Galjot <luka.galjot@gmail.com>
2017-04-28 16:37:44 +0200

    Veliko stvari
    Premaknjenja POST zahteva,
    vpis datuma

--- app/src/main/java/com/lukag/atvoznired/DisplayMessageActivity.java
+++ app/src/main/java/com/lukag/atvoznired/DisplayMessageActivity.java
@@ -4,26 +4,135 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+    public static JSONObject POSTreply = null;
+    private static String[][] vozniRed = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
 
-        // Get the Intent that started this activity and extract the string
+        /*
+        Dobim intent iz drugega activitya
+         */
         Intent intent = getIntent();
+        ArrayList<String> prenos = intent.getStringArrayListExtra(MainActivity.EXTRA_MESSAGE);
+        POSTiT(prenos.get(0), prenos.get(1), prenos.get(2));/*
+        while (POSTreply == null) {
+            Log.d("Strežnik", "Čakam na odziv");
+        }*/
 
-        HashMap<String, String> vozniRed = (HashMap<String,String>)intent.getSerializableExtra(MainActivity.EXTRA_MESSAGE);
-
-        Log.v("HashMapTest", vozniRed.get("ID"));
         // Capture the layout's TextView and set the string as its text
-        //TextView textView = (TextView) findViewById(R.id.textView);
-        //textView.setText(vozniRed.toString());
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(constructMessage());
+    }
+
+    // Kreiram JSON objekt
+    public void SetVozniRed(JSONObject reply) {
+        String status = null;
+        String message = null;
+
+        try {
+            status = reply.get("status").toString();
+            message = reply.get("message").toString();
+            Log.d("Status","Strežnik je vrnil " + status + ": " + message);
+            JSONArray schedule = reply.getJSONArray("schedule");
+
+            vozniRed = new String[schedule.length()][6];
+
+            for (int i = 0; i < schedule.length(); i++) {
+                vozniRed[i][0] = schedule.getJSONObject(i).getString("ID");
+                vozniRed[i][1] = schedule.getJSONObject(i).getString("ODHOD_FORMATED");
+                vozniRed[i][2] = schedule.getJSONObject(i).getString("PRIHOD_FORMATED");
+                vozniRed[i][3] = schedule.getJSONObject(i).getString("KM_POT");
+                vozniRed[i][4] = schedule.getJSONObject(i).getString("CAS_FORMATED");
+                vozniRed[i][5] = schedule.getJSONObject(i).getString("STATUS");
+            }
+        } catch (JSONException e) {
+            Log.e("getResponse", "Napaka v pri parsanju JSON datoteke");
+        }
+    }
+
+    /*
+    Metoda od serverja zahteva podatke o voznem redu
+    */
+    public void POSTiT(final String fromID, final String toID, final String date) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://www.alpetour.si/wp-admin/admin-ajax.php";
+
+        StringRequest POSTrequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            POSTreply = new JSONObject(response);
+                            SetVozniRed(POSTreply);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("ERROR", "error => " + error.toString());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("action", "showRoutes");
+                params.put("fromID", fromID);
+                params.put("toID", toID);
+                params.put("date", date);
+                params.put("general", "false");
+
+                return params;
+            }
+        };
+        queue.add(POSTrequest);
+    }
+
+    public String constructMessage() {
+        String message = "start     stop   km  min  status \n";
+        int length;
+        if (vozniRed != null) {
+            length = vozniRed.length; // Lahko je tudi null
+            Log.d("Dolzina", "Array je dolg: " + length);
+        } else {
+            Log.d("Dolzina", "Array je prazen");
+            length = 0;
+        }
+        for (int i = 0; i < length; i++) {
+            for (int j = 1; j < 6; j++) {
+                if (vozniRed[i][1].length() < 2 && j == 0) {
+                    message += " " + vozniRed[i][j] + "  ";
+                } else {
+                    message += vozniRed[i][j] + "  ";
+                }
+            }
+            message += "\n";
+        }
+    return message;
     }
 }
--- app/src/main/java/com/lukag/atvoznired/MainActivity.java
+++ app/src/main/java/com/lukag/atvoznired/MainActivity.java
@@ -1,59 +1,85 @@
 package com.lukag.atvoznired;
 
+import android.annotation.TargetApi;
 import android.app.AlertDialog;
-import android.content.Context;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.JsonReader;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import android.widget.Toast;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.lukag.atvoznired";
-
-    public JSONObject POSTreply = null;
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private int year, month, day;
+    public static String date = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //prvi spinner
+        // Današnji datum
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat today = new SimpleDateFormat("dd.MM.yyyy");
+        String formattedDate = today.format(c.getTime());
+        EditText text = (EditText)findViewById(R.id.textCalendar);
+        text.setText(formattedDate);
+
+        // Prvi spinner
         Spinner spinner_vstop = (Spinner) findViewById(R.id.vstop_spin);
         ArrayAdapter<CharSequence> adapter_vstop = ArrayAdapter.createFromResource(this,
                 R.array.vstopne_postaje, android.R.layout.simple_spinner_item);
         adapter_vstop.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_vstop.setAdapter(adapter_vstop);
 
-        //drugi spinner
+        // Drugi spinner
         Spinner spinner_izstop = (Spinner) findViewById(R.id.izstop_spin);
         ArrayAdapter<CharSequence> adapter_izstop = ArrayAdapter.createFromResource(this,
                 R.array.izstopne_postaje, android.R.layout.simple_spinner_item);
         adapter_izstop.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_izstop.setAdapter(adapter_izstop);
 
+        // Koledar
+        final Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(24)
+            public void onClick(View v) {
+                new DatePickerDialog(MainActivity.this, myDateListener, year, month, day);
+                calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                day = calendar.get(Calendar.DAY_OF_MONTH);
+            }
+
+            private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker arg0, int year, int month, int day) {
+                            date = day + "." + month + "." + year;
+                        }
+                    };
+        });
+
+        // Gumb za prikaz
         final Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -67,15 +93,19 @@ public class MainActivity extends AppCompatActivity {
                 String vstop_text = spinner_vstop.getSelectedItem().toString(); //podatek od uporabnika
                 String izstop_text = spinner_izstop.getSelectedItem().toString(); //podatek od uporabnika
 
-                Log.d("Vhod","Sem ga dobil: " + vstop_text + " " + izstop_text); //to deluje
+                Log.d("Vhod","Sem ga dobil: " + vstop_text + " " + izstop_text);
 
                 String vstopnaPostaja = null;
                 String izstopnaPostaja = null;
 
+                // Datum
+                EditText text = (EditText)findViewById(R.id.textCalendar);
+                String date = text.getText().toString();
+
                 if (vstop_text.equals(izstop_text)) {
                     new AlertDialog.Builder(MainActivity.this)
-                            .setTitle("Brezveznež")
-                            .setMessage("Prosim vnesi različni postaji!?")
+                            .setTitle("Opozorilo")
+                            .setMessage("Prosim vnesi različni postaji")
                             .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int which) {
                                 }
@@ -94,13 +124,29 @@ public class MainActivity extends AppCompatActivity {
                         Log.e("JSON Parser", "Napaka pri pridobivanju podatkov");
                     }
                     Log.i("Postaje", "Vstopna postaja: " + vstopnaPostaja + " Izstopna postaja: " + izstopnaPostaja);
-                    POSTiT(vstopnaPostaja, izstopnaPostaja, "24.3.2017");
-                }
 
+                    /*
+                    V nov activity pošljem podatke, ki jih je izbral uporabnik
+                    */
+                    ArrayList<String> prenos = new ArrayList<>();
+                    prenos.add(0, vstopnaPostaja);
+                    prenos.add(1, izstopnaPostaja);
+                    prenos.add(2, date);
+                    //prenos.add(0, "0129");
+                    //prenos.add(1, "0855");
+                    //prenos.add(2, "28.4.2017");
+                    Intent intent = new Intent(MainActivity.this, DisplayMessageActivity.class);
+                    intent.putStringArrayListExtra(EXTRA_MESSAGE, prenos);
+                    startActivity(intent);
+                }
             }
         });
     }
 
+    /*
+    Metoda, ki mi iz JSON datoteke prebere vse postaje
+    Vrne mi string v katerem je postaja
+     */
     public String postajeFromAsset(int index) {
         String v_json = null;
         try {
@@ -120,85 +166,5 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
         return v_json;
-    }
-
-    private void writeToFile(String data, Context context) {
-        try {
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("reply.json", Context.MODE_PRIVATE));
-            outputStreamWriter.write(data);
-            outputStreamWriter.close();
-        }
-        catch (IOException e) {
-            Log.e("Exception", "File write failed: " + e.toString());
-        }
-    }
-
-    public void SetVozniRed(JSONObject reply) {
-
-        HashMap<String, String> vozniRed = new HashMap<>();
-        vozniRed.put("key", "value");
-        String status = null;
-        String message = null;
-
-        try {
-            status = reply.get("status").toString();
-            message = reply.get("message").toString();
-            Log.d("Status","Strežnik je vrnil " + status + ": " + message);
-            JSONArray schedule = reply.getJSONArray("schedule");
-
-            for (int i = 0; i < schedule.length(); i++) {
-                vozniRed.put("ID", schedule.getJSONObject(i).getString("ID"));
-                vozniRed.put("ODHOD_FORMATED", schedule.getJSONObject(i).getString("ODHOD_FORMATED"));
-                vozniRed.put("PRIHOD_FORMATED", schedule.getJSONObject(i).getString("PRIHOD_FORMATED"));
-                vozniRed.put("KM_POT", schedule.getJSONObject(i).getString("KM_POT"));
-                vozniRed.put("CAS_FORMATED", schedule.getJSONObject(i).getString("CAS_FORMATED"));
-                vozniRed.put("STATUS", schedule.getJSONObject(i).getString("STATUS"));
-            }
-        } catch (JSONException e) {
-            Log.e("getResponse", "Napaka v pri parsanju JSON datoteke");
-        }
-
-        Intent intent;
-        intent = new Intent(MainActivity.this, DisplayMessageActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, vozniRed);
-        startActivity(intent);
-    }
-
-    public void POSTiT(final String fromID, final String toID, final String date) {
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://www.alpetour.si/wp-admin/admin-ajax.php";
-
-        StringRequest POSTrequest = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            POSTreply = new JSONObject(response);
-                            SetVozniRed(POSTreply);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d("ERROR","error => "+error.toString());
-                    }
-                }
-        ) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                params.put("action", "showRoutes");
-                params.put("fromID", fromID);
-                params.put("toID", toID);
-                params.put("date", date);
-                params.put("general", "false");
-
-                return params;
-            }
-        };
-        queue.add(POSTrequest);
     }
 }
