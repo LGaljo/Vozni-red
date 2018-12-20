d9bb5ee402f6778cd42cb704cf83b54ec8e80424
Luka Galjot <luka.galjot@gmail.com>
2018-12-20 15:39:46 +0100

    SwipeBackHelper na strani o aplikaciji

--- app/src/main/java/com/lukag/atvoznired/DisplayAppInfo.java
+++ app/src/main/java/com/lukag/atvoznired/DisplayAppInfo.java
@@ -3,13 +3,18 @@ package com.lukag.atvoznired;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.jude.swipbackhelper.SwipeBackHelper;
 
 public class DisplayAppInfo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_info);
+
+        SwipeBackHelper.onCreate(this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.VozniRedToolbar);
         toolbar.setTitle(R.string.about);
@@ -26,5 +31,33 @@ public class DisplayAppInfo extends AppCompatActivity {
             e.printStackTrace();
             version.setText("err");
         }
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        SwipeBackHelper.onPostCreate(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SwipeBackHelper.onDestroy(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return false;
     }
 }
