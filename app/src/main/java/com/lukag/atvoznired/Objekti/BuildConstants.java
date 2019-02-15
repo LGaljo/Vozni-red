27d6adc33a279f30e6b4801618a902872e3607aa
Luka Galjot <luka.galjot@gmail.com>
2019-02-15 23:26:58 +0100

    Make BuildConstants a singleton class

--- app/src/main/java/com/lukag/atvoznired/Objekti/BuildConstants.java
+++ app/src/main/java/com/lukag/atvoznired/Objekti/BuildConstants.java
@@ -5,4 +5,18 @@ import java.util.HashMap;
 public class BuildConstants {
     final static public String tokenKey = "R300_VozniRed_2015";
     public static HashMap<String, String> seznamPostaj = new HashMap<>();
+    public static Relacija relacija;
+
+    public static BuildConstants buildConstants;
+
+    private BuildConstants() {}
+
+    public static BuildConstants getInstance() {
+        if (buildConstants == null) {
+            buildConstants = new BuildConstants();
+        }
+        return buildConstants;
+    }
+
+
 }
