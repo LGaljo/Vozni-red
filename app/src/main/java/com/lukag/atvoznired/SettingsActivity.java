395b5a3ef37adac6a5fe24f7f8d39a2b024b1ce3
Luka Galjot <luka.galjot@gmail.com>
2019-02-17 19:30:52 +0100

    Nekaj sprememb v navigation drawerju in nastavitvah

--- app/src/main/java/com/lukag/atvoznired/SettingsActivity.java
+++ app/src/main/java/com/lukag/atvoznired/SettingsActivity.java
@@ -2,6 +2,11 @@ package com.lukag.atvoznired;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+
+import com.jude.swipbackhelper.SwipeBackHelper;
 
 public class SettingsActivity extends AppCompatActivity {
     public static final String ISKANJE_S_SUMNIKI = "sumniki";
@@ -10,8 +15,46 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        getLayoutInflater().inflate(R.layout.toolbar, (ViewGroup)findViewById(android.R.id.content));
+        Toolbar toolbar = (Toolbar)findViewById(R.id.VozniRedToolbar);
+        toolbar.setTitle(R.string.settings);
+        toolbar.setTitleTextAppearance(getApplicationContext(), R.style.ToolbarTitle);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        setSupportActionBar(toolbar);
+
+        SwipeBackHelper.onCreate(this);
+
+
         getSupportFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        SwipeBackHelper.onPostCreate(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SwipeBackHelper.onDestroy(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/lukag/atvoznired/SettingsFragment.java
+++ app/src/main/java/com/lukag/atvoznired/SettingsFragment.java
@@ -1,12 +1,8 @@
 package com.lukag.atvoznired;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
